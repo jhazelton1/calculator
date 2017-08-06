86f43a9387a790bd0f1fc0d92f837f37a1ae9a2c
Nicholas Hudson <Nicholas Hudson>
2017-08-05 22:48:32 -0400

    Fourth Commit

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,20 @@
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const button = document.querySelectorAll('.button');
+
+// onload = function(e) {
+//     buttons = document.getElementsByClassName('button');
+//     for(i=0; i<buttons.length; i++) {
+//         buttons[i].addEventListener('click', function() {document.getElementById('inputOutput').innerText += document.getElementById(button[i].).innerText});
+//     }
+// }
+
+function replyClick(clickedID) {
+    alert(clickedID);
+}
+
+// button.addEventListener('click', function(){
+//     console.log("clicked: " + event.target);
+// });
